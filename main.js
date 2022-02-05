b903e058fdd6f3c7bcb6a7001b061ece93daa6f9
Dima <dima.tsymbal12345@gmail.com>
2022-02-05 15:44:58 +0200

    first step app done

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const popularMovies = "https://api.themoviedb.org/3/discover/movie";
-
 class Movies {
   constructor() {
-    this.allMovies =
-      "https://api.themoviedb.org/3/movie/550?api_key=060f347dcc7582a34aa7ccbecd45da16";
-  }
+    this.imgPath = "https://image.tmdb.org/t/p/w1280"
+    this.popularMovies = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=060f347dcc7582a34aa7ccbecd45da16&page=1"
+    this.search = "https://api.themoviedb.org/3/search/movie?api_key=060f347dcc7582a34aa7ccbecd45da16&query='"
+
+}
 
   async getMovies() {
     try {
-      let request = await fetch(this.allMovies);
+      let request = await fetch(this.popularMovies);
 
       if (request.ok) {
         return await request.json();
@@ -23,11 +23,13 @@ class Movies {
  async getData() {
      let data = await this.getMovies()
      console.log(data)
+     
  }
   render() {
    let list =  this.getData()
-
-   
+    for(let key in list) {
+        console.log(`key = ${key} property = ${list[key]}`)
+    }
   }
 }
 
