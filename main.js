a4dfc7abdb2a6945e4a50884a26125524383f91a
Dima <dima.tsymbal12345@gmail.com>
2022-03-08 11:24:54 +0200

    refact

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const form = document.getElementById('form');
-const main = document.querySelector('.main')
+const form = document.getElementById("form");
+const main = document.querySelector(".main");
 class Movies {
   constructor() {
     this.imgPath = "https://image.tmdb.org/t/p/w1280";
@@ -7,16 +7,15 @@ class Movies {
       "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=060f347dcc7582a34aa7ccbecd45da16&page=1";
     this.search =
       "https://api.themoviedb.org/3/search/movie?api_key=060f347dcc7582a34aa7ccbecd45da16&query=";
-    this.data = [];
     this.searchReq = document.querySelector(".search");
   }
 
-  async getMovies(url) {
+   async getMovies(url) {
     try {
       let request = await fetch(url);
 
       if (request.ok) {
-        return await request.json();
+        return request.json();
       } else {
         console.error(request.error);
       }
@@ -26,81 +25,106 @@ class Movies {
   }
 
   async getPopular() {
-    let data = await this.getMovies(this.popularMovies);
-    for (const item of data.results) {
-      this.data.push(item);
+    let result;
+    try {
+      result = await this.getMovies(this.popularMovies);
+    } catch (error) {
+      console.log(new Error(error));
     }
-   
-    console.log(this.data)
+
+    return result;
   }
- 
 
   async searchMovie() {
-    if (this.searchReq && this.searchReq.value !== "") {
-      let res = await this.getMovies(`${this.search + this.searchReq.value}`);
-      console.log("res", res);
+    let result;
+    try {
+      if (this.searchReq && this.searchReq.value !== "") {
+        result = await this.getMovies(`${this.search + this.searchReq.value}`);
+      } else {
+        return
+      }
+    } catch (error) {
+      console.log(new Error(error));
+    }
+    return result
+  }
+
+
+
+
+  voteRate(vote) {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 5) {
+      return "orange";
     } else {
-      window.location.reload();
+      return "red";
     }
   }
-  render() {
-    this.getPopular();
 
-    if(!this.data) {
-      main.innerHTML = '<p> Wait Data...</p>'
-    } else {
-     setTimeout(() => {
+  showMovieInPage(movie) {
+      if (!movie) {
+          main.innerHTML = '<p class="error"> Wait Data...</p>';
+        } else {
+          movie.forEach((item) => {
+            const {
+              poster_path,
+              original_title,
+              vote_average,
+              overview,
+              id,
+              title,
+            } = item;
 
+            const movieEl = document.createElement("div");
+            movieEl.setAttribute('data-id', `${id}`)
 
-      this.data.forEach(item => {
-        const {poster_path, original_title, vote_average, overview, id, title} = item
+            movieEl.classList.toggle("movie");
 
-        const movieEl = document.createElement('div')
-
-        movieEl.classList.toggle('movie')
-
-       movieEl.innerHTML = 
-        `
-          <div class="movie" data-id = "${id}">
-              <img src="${this.imgPath + poster_path}" alt="photo">
+            movieEl.innerHTML = `
               
-              <div class="movie-info">
-                  <h3>${original_title}</h3>
-                  <span class="${voteRate(vote_average)}"> ${vote_average} </span>
-              </div>
-              <div class="overview">
-                  <h3>${title}</h3>
-                  ${overview}
-              </div>
-          </div>
-        `
+                  <img src="${this.imgPath + poster_path}" alt="photo">
+                  
+                  <div class="movie-info">
+                      <h3>${original_title}</h3>
+                      <span class="${this.voteRate(
+                        vote_average
+                      )}"> ${vote_average} </span>
+                  </div>
+                  <div class="overview">
+                      <h3>${title}</h3>
+                      ${overview}
+                  </div>
+              
+            `;
+            main.appendChild(movieEl);
+          });
+        }
+     
+  }
 
-     main.appendChild(movieEl)
-      })
- 
+  renderSearch() {
+        this.searchMovie()
+          .then(data => data.results)
+            .then(result => this.showMovieInPage(result))
+  }
 
-     }, 1000) 
-    }
+  render() {
+    
 
+
+    this.getPopular()
+      .then(data => data.results)
+        .then(movies => this.showMovieInPage(movies))
   }
 }
 
-function voteRate (vote) {
-  if(vote >= 8) {
-    return 'green'
-  } else if( vote >= 5) {
-    return 'orange'
-  } else {
-    return 'red'
-  }
-}
 
-const mov = new Movies();
-mov.render()
 
+const moviePage = new Movies();
+moviePage.render()
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  mov.searchMovie();
+  e.preventDefault()
+  moviePage.renderSearch()
 });
